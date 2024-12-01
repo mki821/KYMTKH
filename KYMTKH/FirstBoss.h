8056dc1f8afabc970d57bf4a04b70291a55a9ad4
youtaehew <youtaehew123@gmail.com>
2024-12-02 00:42:48 +0900

    add: ChaseProj

--- KYMTKH/FirstBoss.h
+++ KYMTKH/FirstBoss.h
@@ -12,18 +12,18 @@ public:
 	virtual void Render(HDC hdc) override;
 private:
 	void One();
-	void CircleShot(int shotDeg);
+private:
+	void StopAndChaseShot(int shotDeg, float speed);
+	void CircleShot(int shotDeg, float speed);
 	void SpreadShot(int count);
 public:
 	const bool GetScalceUp() const { return m_scaleUp; }
+	const Object* GetPlayer() const { return m_player; }
 private:
 	float m_timer = 0.0f;
 	int m_angleModifier = 0;
 	bool m_scaleUp = false;
 
 	const Object* m_player;
-
-	int bulletCount = 4;
-	bool isStop = false;
 };
 
